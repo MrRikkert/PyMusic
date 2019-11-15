81de7ba09d4f340be70211136b0aed74699ba0b1
Rik Hendrikx <hendrikx.rik@gmail.com>
2019-11-15 14:50:51 +0100

    renamed table name of UserDb

--- app/db/models.py
+++ app/db/models.py
@@ -4,6 +4,7 @@ from app.db.base import db
 
 
 class UserDb(db.Entity):
+    _table_ = "user"
     id: int = PrimaryKey(int, auto=True)
     username: str = Required(str, unique=True)
     email: str = Required(str, unique=True)
