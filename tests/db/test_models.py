0c169ab2a647c71db0bd17c83f19321971c26f6c
MrRikkert <hendrikx.rik@gmail.com>
2019-11-15 22:53:20 +0100

    reset db with each test

--- tests/db/test_models.py
+++ tests/db/test_models.py
@@ -1,15 +1,21 @@
-from pony.orm import db_session
-from app.db.base import db
-from app.db.models import UserDb
 from pony import orm
+from pony.orm import db_session
+
+from app.db.models import UserDb
+from tests.utils import reset_db
 
 
 def setup_function():
-    db.bind(provider="sqlite", filename=":memory:")
-    db.generate_mapping(create_tables=True)
+    reset_db()
 
 
 @db_session
 def test_test():
     UserDb(username="test", email="test", password="test")
     assert orm.count(u for u in UserDb) == 1
+
+
+@db_session
+def test_test2():
+    UserDb(username="test", email="test", password="test")
+    assert UserDb[1] is not None
---
+++ tests/utils.py
@@ -0,0 +1,20 @@
+from pony import orm
+
+from app.db.base import db
+
+
+def reset_db():
+    """Resets db by dropping all columns with data and regenerating the schema.
+    Note!
+    ---
+    Does not reset auto_increment keys!
+    """
+    try:
+        db.bind(provider="sqlite", filename=":memory:")
+    except orm.core.BindingError:
+        pass
+    else:
+        db.generate_mapping(check_tables=False)
+
+    db.drop_all_tables(with_all_data=True)
+    db.create_tables()
