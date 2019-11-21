738afbd4b3d37750907d27de4ec1882e0589a772
Rik Hendrikx <hendrikx.rik@gmail.com>
2019-11-21 13:07:57 +0100

    removed a comment

--- app/middleware.py
+++ app/middleware.py
@@ -2,7 +2,7 @@ from pony.orm import db_session
 from starlette.requests import Request
 from app.server import app
 
-# wrap every request in a PonyORM 'db_session'
+
 @app.middleware("http")
 async def db_middleware(request: Request, call_next):
     """Wraps every API call in a PonyORM db_session"""
