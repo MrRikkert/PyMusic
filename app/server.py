6854bd40e097ba6da1621505269ea3f74c613f5f
Rik Hendrikx <hendrikx.rik@gmail.com>
2019-11-26 16:21:44 +0100

    some configuration

--- app/server.py
+++ app/server.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI
 
 from app.routers import user
 
-app = FastAPI()
+app = FastAPI(
+    title="PyMusic", docs_url="/api/docs/swagger", redoc_url="/api/docs/redoc"
+)
 
 app.include_router(user.router, prefix="/user", tags=["user"])
