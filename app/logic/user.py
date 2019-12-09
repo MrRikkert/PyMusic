e50cbf347feae901e80569ef29fe515b35291b10
Rik Hendrikx <hendrikx.rik@gmail.com>
2019-12-09 08:40:09 +0100

    validate email exists

--- app/logic/user.py
+++ app/logic/user.py
@@ -28,12 +28,14 @@ def register(register: RegisterIn) -> UserDb:
     """
     register = register.copy(deep=True)
     register.password = hash_password(register.password)
-    if exists(register.username):
+    if username_exists(register.username):
         raise IntegrityError("Username already exists")
+    if email_exists(register.email):
+        raise IntegrityError("Email already exists")
     return UserDb(**register.dict())
 
 
-def exists(username: str) -> bool:
+def username_exists(username: str) -> bool:
     """Check if the user already exists in the database
 
     ## Arguments:
@@ -50,18 +52,28 @@ def exists(username: str) -> bool:
     return True
 
 
-def get(username: str) -> UserDb:
+def email_exists(email: str) -> bool:
+    user = get(email)
+    if user is None:
+        return False
+    return True
+
+
+def get(value: str) -> UserDb:
     """Get user from database
 
     ## Arguments:
-    - `username`: `str`:
-        - Name of the user
+    - `value`: `str`:
+        - Name or email of the user, if the value contains '@' it is used as an email
 
     ## Returns:
     - `UserDb`:
         - The found user. Returns `None` when no user is found
     """
-    return UserDb.get(username=username)
+    if value.find("@") == -1:
+        return UserDb.get(username=value)
+    else:
+        return UserDb.get(email=value)
 
 
 def authenticate(username: str, password: str) -> UserDb:
