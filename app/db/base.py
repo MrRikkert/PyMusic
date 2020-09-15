b7d95b1e47a13f4737db40874f595090fc6d237c
MrRikkert <hendrikx.rik@gmail.com>
2020-09-16 00:23:28 +0200

    removed leftover from last commit (oops)

--- app/db/base.py
+++ app/db/base.py
@@ -1,10 +1,3 @@
 from pony import orm
 
 db = orm.Database()
-
-
-@db.on_connect(provider="sqlite")
-def sqlite_case_sensitivity(db, connection):
-    cursor = connection.cursor()
-    cursor.execute("PRAGMA case_sensitive_like = OFF")
-
