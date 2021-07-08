a9e0c0e42125d51bdbd982c850b084cc4263cf19
MrRikkert <hendrikx.rik@gmail.com>
2021-07-08 14:21:11 +0200

    plays over time compare previous time frame

--- app/dash/components/plays_over_time_chart.py
+++ app/dash/components/plays_over_time_chart.py
@@ -1,4 +1,5 @@
 import dash_bootstrap_components as dbc
+import dash_core_components as dcc
 import pandas as pd
 import plotly.express as px
 from app.dash.app import app
@@ -11,8 +12,8 @@ from app.dash.utils import (
 )
 from app.db.base import db
 from dash.dependencies import Input, Output
+from dateutil.relativedelta import relativedelta
 from pony.orm import db_session
-import dash_core_components as dcc
 
 
 def get_layout():
@@ -29,7 +30,15 @@ def get_layout():
 
 
 def _build_query(date_range, min_date, max_date, filter_date=True):
-    if date_range == "week" or date_range == "month":
+    if date_range == "week":
+        select = """
+            EXTRACT(year from DATE) AS "Year",
+            EXTRACT(month from DATE) AS "Month",
+            EXTRACT(day from DATE) AS "Day",
+            EXTRACT(week from DATE)::varchar(255) AS "Week",
+        """
+        group_columns = ['"Year"', '"Month"', '"Day"', '"Week"']
+    elif date_range == "month":
         select = """
             EXTRACT(year from DATE) AS "Year",
             EXTRACT(month from DATE) AS "Month",
@@ -38,7 +47,7 @@ def _build_query(date_range, min_date, max_date, filter_date=True):
         group_columns = ['"Year"', '"Month"', '"Day"']
     elif date_range == "year":
         select = """
-            EXTRACT(year from DATE) AS "Year",
+            EXTRACT(year from DATE)::varchar(255) AS "Year",
             EXTRACT(month from DATE) AS "Month",
         """
         group_columns = ['"Year"', '"Month"']
@@ -63,8 +72,21 @@ def _build_query(date_range, min_date, max_date, filter_date=True):
     return sql
 
 
-def _get_data(date_range, min_date, max_date, filter_date=True):
-    sql = _build_query(date_range, min_date, max_date, filter_date)
+@app.callback(
+    Output("plays-line-chart", "figure"),
+    Input("date-range-select", "value"),
+    Input("date-select", "value"),
+)
+@set_theme
+@convert_dates
+@db_session
+def _plays_bar_chart(date_range, min_date, max_date):
+    sql = _build_query(date_range, min_date, max_date)
+
+    if date_range == "week":
+        min_date = min_date - relativedelta(days=7)
+    elif date_range == "year":
+        min_date = min_date - relativedelta(years=1)
 
     df = pd.read_sql_query(
         sql,
@@ -75,29 +97,25 @@ def _get_data(date_range, min_date, max_date, filter_date=True):
     df = df.sort_values("Date")
 
     if date_range == "week":
-        df["Date"] = df["Date"].dt.strftime("%a, %b %d")
+        df["Date"] = df["Date"].dt.strftime("%a")
+        color = "Week"
     elif date_range == "month":
         df["Date"] = df["Date"].dt.strftime("%d")
+        color = None
     elif date_range == "year":
         df["Date"] = df["Date"].dt.strftime("%b")
+        color = "Year"
 
     df, scale = set_length_scale(df, "Time")
-    return (df, scale)
-
-
-@app.callback(
-    Output("plays-line-chart", "figure"),
-    Input("date-range-select", "value"),
-    Input("date-select", "value"),
-)
-@set_theme
-@convert_dates
-@db_session
-def _plays_bar_chart(date_range, min_date, max_date):
-    df, scale = _get_data(date_range, min_date, max_date)
 
     fig = px.bar(
-        df, x="Date", y="Time", title=f"Playtime over time ({scale})", text="Time"
+        df,
+        x="Date",
+        y="Time",
+        title=f"Playtime over time ({scale})",
+        text="Time",
+        color=color,
+        barmode="group",
     )
     fig.update_layout(
         yaxis_title=f"Total Playtime ({scale})",
