ffbdef1001978324bb529b2cc669273a8d01b132
MrRikkert <hendrikx.rik@gmail.com>
2021-07-14 21:38:00 +0200

    change order of cards on mobile

--- dash_app.py
+++ dash_app.py
@@ -22,42 +22,60 @@ app.layout = html.Div(
         navbar.get_layout(),
         dbc.Container(
             [
-                dbc.Row(dbc.Col(title_bar.get_layout(), md=12)),
+                dbc.Row(dbc.Col(title_bar.get_layout(), xs=12)),
                 dbc.Row(
                     [
-                        dbc.Col(stats.get_layout("total_scrobbles"), md=12, lg=3),
-                        dbc.Col(stats.get_layout("daily_scrobbles"), md=12, lg=3),
-                        dbc.Col(stats.get_layout("daily_playtime"), md=12, lg=3),
-                        dbc.Col(stats.get_layout("total_playtime"), md=12, lg=3),
+                        dbc.Col(stats.get_layout("total_scrobbles"), xs=12, lg=3),
+                        dbc.Col(stats.get_layout("daily_scrobbles"), xs=12, lg=3),
+                        dbc.Col(stats.get_layout("daily_playtime"), xs=12, lg=3),
+                        dbc.Col(stats.get_layout("total_playtime"), xs=12, lg=3),
                     ]
                 ),
-                dbc.Row([dbc.Col(plays_over_time_chart.get_layout(), md=12)]),
+                dbc.Row([dbc.Col(plays_over_time_chart.get_layout(), xs=12)]),
                 dbc.Row(
                     [
                         dbc.Col(
-                            top_chart.get_layout("mixed", reverse=True), md=12, lg=9
+                            top_image.get_layout("series"),
+                            xs={"size": 12, "order": "first"},
+                            lg={"size": 3, "order": "last"},
                         ),
-                        dbc.Col(top_image.get_layout("series"), md=12, lg=3),
-                    ]
-                ),
-                dbc.Row(
-                    [
-                        dbc.Col(top_image.get_layout("artist"), md=12, lg=3),
                         dbc.Col(
-                            top_chart.get_layout("artist", reverse=False), md=12, lg=9
+                            top_chart.get_layout("mixed", reverse=True),
+                            xs={"size": 12, "order": "last"},
+                            lg={"size": 9, "order": "first"},
                         ),
                     ]
                 ),
                 dbc.Row(
                     [
                         dbc.Col(
-                            top_chart.get_layout("album", reverse=True), md=12, lg=9
+                            top_image.get_layout("artist"),
+                            xs={"size": 12, "order": "first"},
+                            lg={"size": 3, "order": "first"},
                         ),
-                        dbc.Col(top_image.get_layout("album"), md=12, lg=3),
+                        dbc.Col(
+                            top_chart.get_layout("artist", reverse=False),
+                            xs={"size": 12, "order": "last"},
+                            lg={"size": 9, "order": "first"},
+                        ),
                     ]
                 ),
-                dbc.Row([dbc.Col(vocal_chart.get_layout(), md=12)]),
-                dbc.Row([dbc.Col(listening_clock_chart.get_layout(), md=12, lg=4)]),
+                dbc.Row(
+                    [
+                        dbc.Col(
+                            top_image.get_layout("album"),
+                            xs={"size": 12, "order": "first"},
+                            lg={"size": 3, "order": "last"},
+                        ),
+                        dbc.Col(
+                            top_chart.get_layout("album", reverse=True),
+                            xs={"size": 12, "order": "last"},
+                            lg={"size": 9, "order": "first"},
+                        ),
+                    ]
+                ),
+                dbc.Row([dbc.Col(vocal_chart.get_layout(), xs=12)]),
+                dbc.Row([dbc.Col(listening_clock_chart.get_layout(), xs=12, lg=4)]),
             ],
             fluid=False,
             id="main-content",
