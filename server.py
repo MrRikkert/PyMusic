ffcefed8ae9f0dd316e0ec892ad86d543b38863f
MrRikkert <hendrikx.rik@gmail.com>
2022-01-21 19:42:39 +0100

    moved init_db to if main

--- server.py
+++ server.py
@@ -4,8 +4,8 @@ from app import pages
 from app.app import app, server  # noqa
 from shared.db.base import init_db
 
-init_db()
 app.layout = pages.get_layout()
 
 if __name__ == "__main__":
+    init_db()
     app.run_server(debug=True)
