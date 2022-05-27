a5c6c298f0099385273109b3646e55e7e0ea5f9e
MrRikkert <MrRikkert+github@protonmail.com>
2022-05-27 23:17:01 +0200

    make gunicorn run properly in docker

--- server.py
+++ server.py
@@ -9,11 +9,16 @@ from app.pages.listening_report import get_layout  # noqa: E402
 from shared.db.base import init_db  # noqa: E402
 
 
-def run_server(*arg, **kwarg):
+def init():
     app.layout = get_layout()
     init_db()
-    app.run_server(debug=True)
+
+
+def run_server():
+    init()
+    return server
 
 
 if __name__ == "__main__":
-    run_server()
+    init()
+    app.run_server(debug=True)
